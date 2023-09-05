702159dc02387a602c64cd980b90ced51fcb2e5b
DevanshiAgnihotri <79466634+DevanshiAgnihotri@users.noreply.github.com>
2023-09-05 16:08:03 -0400

    Update False_Gen_Secrets.py

--- False_Gen_Secrets.py
+++ False_Gen_Secrets.py
@@ -1,37 +1,30 @@
+#False GH PAT tokens
+
 fake_ghpat_1 = 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
-
 fake_ghpat_2 = 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
-
 fake_ghpat_3 = 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
-
 fake_ghpat_4 = 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
-
 fake_ghpat_5 = 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
-
 fake_ghpat_6 = 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
-
 fake_ghpat_7 = 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
-
 fake_ghpat_8 = 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
-
 fake_ghpat_9 = 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
-
 fake_ghpat_10 = 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
 -------------------------------------------------------------
+#False Gmail API tokens
 
 fake_gmailapi_1 = AIzaSyATLLIRPINfpZpRnhzHHckDyTrrgL9VBGQ
-fake_gmailapi_2
-fake_gmailapi_3
-fake_gmailapi_4
-fake_gmailapi_5
-fake_gmailapi_6
-fake_gmailapi_7
-fake_gmailapi_8
-fake_gmailapi_9
-fake_gmailapi_10
+fake_gmailapi_2 = AIzaSyBBXgC_pcOUZVRM0ul8mRSk7Sc4i-FkLjc
+fake_gmailapi_3 = AIzaSyAXOhq7KhjkxBq6OU-Cp9OXha1TrA696NU
+fake_gmailapi_4 = AIzaSyCo-YTaDVNOJf4lmpGqLVzoQpn1bqgneAg
+fake_gmailapi_5 = AIzaSyAVsTzapC79TvS_uNCZNqfGHolPHIDQy38
+fake_gmailapi_6 = AIzaSyCC_8QmoaLjUX7GnRu_4--GybM0sv45O_c
+fake_gmailapi_7 = AIzaSyC2lzjAuYAAe_RP1ko8smrsiwKhi0dxDkU
+fake_gmailapi_8 = AIzaSyAPFMHMhAP1ErjjF7nXCCG3IK2Pzb2gyYs
+fake_gmailapi_9 = AIzaSyDNfwrJr-OgVq8Nc8EMSthxKFgebkAPwn4
+fake_gmailapi_10 = AIzaSyDhZC1br5F6yw-MksjngDu9wX7t7bt2o5Q
+---------------------------------------------------------------
 
-{
-  "username": "proprietary_creds_format",
-  "base64_password": "25Pa4c+Ckmdgz5RF"
-}
+
+
 #define IO_KEY       "aio_AbCD37pXDJeJvBsNuyf6l6RYqoCb"

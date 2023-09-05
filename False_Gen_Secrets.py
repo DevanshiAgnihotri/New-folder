d6f3c170ee794bfc576b0828e51d1e3c98f7b3cc
DevanshiAgnihotri <79466634+DevanshiAgnihotri@users.noreply.github.com>
2023-09-05 15:19:09 -0400

    Update False_Gen_Secrets.py

--- False_Gen_Secrets.py
+++ False_Gen_Secrets.py
@@ -1,24 +1,22 @@
-azure_ad_key1 = "p9m-kN6B-l5D7oZ4qR0n-A1tV8c3X2fU"
-azure_ad_key2 = 'y8v-bW4N-p5E2oC1qL0r-F9uT6xK7zJ3'
-azure_ad_key3 = 'a0t-lF7U-q9D8oX2nB1-C5vR6zP3mK4h'
-azure_ad_key4 = 'c3u-aV4R-l5P1oF2nE9-T8qZ0xK7zJ6b'
-azure_ad_key5 = 'd5x-kM0S-v8N1oL4nT7-A2pC6zR9tF3j'
-azure_ad_key6 = 'z1v-bE3Q-r5D2oG6nF4-A9tZ0c8xP7mK'
-azure_ad_key7 = 'q9j-dT5F-s2B4oR3nA7-E1tV6zP0xK8l'
-azure_ad_key8 = 'w8a-kN3C-v5R2oT4nB9-A1tX6zY7pJ0h'
-azure_ad_key9 = 's2c-tF1E-r9W4oX6nA8-L5vH3zP0xJ7m'
-azure_ad_key10 = 'r9b-kN2G-t8P6oU3nD1-A4sV7zL0xJ5h'
+fake_ghpat_1 = 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
 
-access_token = 'ghp_b7f776e7fa5d4fddb50bf69cfad8d846e'
-access_token = 'ghp_e8d7f7d23c9e5a1cb9600f8d1203b12d7'
-access_token = 'ghp_e9b3c2a8e9cb42479f107ae4184f7d2db'
-access_token = 'ghp_d99dda190dca4884b10c1a5451b5078fa'
-access_token = 'ghp_2fbe9708be8c461aa2f023d1de1a9ff4f'
-access_token = 'ghp_4233ebd26b5a4bb49f423c55c7a1aa1ee'
-access_token = 'ghp_8d2d30f6f5f44dc8a3c9900166ef55c57'
-access_token = 'ghp_f97415f4475a4e0392a000246aabd7612'
-access_token = 'ghp_dc0b99b5d84e411f897c9d88b8f2fd3d3'
-access_token = 'ghp_8228b8d2674d43a9b85a7a44559c8c6b0'
+fake_ghpat_2 = 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
+
+fake_ghpat_3 = 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
+
+fake_ghpat_4 = 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
+
+fake_ghpat_5 = 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
+
+fake_ghpat_6 = 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
+
+fake_ghpat_7 = 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
+
+fake_ghpat_8 = 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
+
+fake_ghpat_9 = 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
+
+fake_ghpat_10 = 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
 
 {
   "username": "proprietary_creds_format",

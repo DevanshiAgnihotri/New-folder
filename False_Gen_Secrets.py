c719b41f5488091acfe02c14891ec644f76e472a
DevanshiAgnihotri <79466634+DevanshiAgnihotri@users.noreply.github.com>
2023-09-13 12:13:59 -0400

    Update False_Gen_Secrets.py

--- False_Gen_Secrets.py
+++ False_Gen_Secrets.py
@@ -31,21 +31,65 @@ fake_twitter_1 = jhBOgbyK3SRapJ5mx2GLdRkqd
 fake_twitter_2 = aTDZTK4a3Wo9Sl7K7x6ycU6aaZOLRB8Ouhv8v1ryHPpZJAYrGJ
 
 --------------------------------------------------------------------
-#False AWS API tokens
+#False AWS Access and Secret tokens
 
 fake_aws_1 = AKIAYDDIOZFUJRIHOHPE
 fake_aws_2 = AKIAYDDIOZFUFYVNWLHQ
 fake_aws_3 = ZK6tcWbB7KUigW9UTYEuD1duPVyobJOrz3CZ0fep
-fake_aws_4 =
-fake_aws_5 =
-fake_aws_6 =
-fake_aws_7 =
-fake_aws_8 =
-fake_aws_9 =
-fake_aws_10 = 
+fake_aws_4 = AKIAYDDIOZFUP45Q6XJ4
+fake_aws_5 = 1ynTNyeYfi9FxJXMpFtAFiTch2d/jg90uE+xDoP6
+fake_aws_6 = AKIAYDDIOZFUGROR7NAH
+fake_aws_7 = KDxz6sc5j1QREW0u6jGjuTLKpflUgv8OGI22QNFs
+fake_aws_8 = AKIAYDDIOZFUPG2BK5Z2
+fake_aws_9 = zHP+Cjpw6D/ojLwZkPOHXV0s8f1qxFvbBD8rlr6f
+fake_aws_10 = AKIAYDDIOZFUA3KUNW7X
+fake_aws_11 = LgZmUxHrHdRgBjvMvs34LdIq6p23NczYRY219EqE
+fake_aws_12 = AKIAYDDIOZFUDY6K66UH
+fake_aws_13 = KJL45yLlVLbNCy0ndB3IG8x9fWVpVxW9bUYaPqbD
+fake_aws_14 = AKIAYDDIOZFUB5XYFE6K
+fake_aws_15 = N/kyQWqfcSo7JzGZoRw4+hQGQd3uGzl9K3lXVBjt
+fake_aws_16 = AKIAYDDIOZFUIK7TAOKH
+fake_aws_17 = 8CvG0EGaXHyVN31bsc1cBQHHrI6PaONpHq+Bwozv
+fake_aws_18 = AKIAYDDIOZFUJTYVMF4B
+fake_aws_19 = 5mFbcNmsDtjpvVv5gl6H5oWTlYZxLUne5hVpa5xr
+--------------------------------------------------------------
+# False CloudFront Key Pair
 
-
-
-
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt79rAPVXHYRKQ3Eq6wcq
+mxawX1ssL2gcn3WW2UnmwkTGOHhPDqh1sdJ3KFtX9w+nb8R97zDuBJ18la795AqQ
+QwH28aiopKL1Pf6Im+QY/kSChJuSYcBojpHpPb8lw6C7PJhCoTpo1SCYOeHsvmSG
+65BNPnQAN45YyTS7JdyIBDZqGMiC0ljnEquFmArNMyHIQCAFLSstSZ+cd8ADObiR
+xNc69N1/e209/K2s+YVyAgCbBblcrGr4LEmI/ykh7A/3DXtpVU/eERs7qwJWSVn+
+r8CVtuzg/K6I2y4z2PoFQmaOilUaau/5DZySex3uvGwaRB31dFQZIkQ1mhkkXuyQ
+wwIDAQAB
+-----END PUBLIC KEY-----
+  -----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEAt79rAPVXHYRKQ3Eq6wcqmxawX1ssL2gcn3WW2UnmwkTGOHhP
+Dqh1sdJ3KFtX9w+nb8R97zDuBJ18la795AqQQwH28aiopKL1Pf6Im+QY/kSChJuS
+YcBojpHpPb8lw6C7PJhCoTpo1SCYOeHsvmSG65BNPnQAN45YyTS7JdyIBDZqGMiC
+0ljnEquFmArNMyHIQCAFLSstSZ+cd8ADObiRxNc69N1/e209/K2s+YVyAgCbBblc
+rGr4LEmI/ykh7A/3DXtpVU/eERs7qwJWSVn+r8CVtuzg/K6I2y4z2PoFQmaOilUa
+au/5DZySex3uvGwaRB31dFQZIkQ1mhkkXuyQwwIDAQABAoIBAAsWW0wPAccvrNV9
+9iOBZrI9/715QGTJ6PQ8QbJx0aFGShmtNMQDKTrmIreGL3wiby2A00KEFtCjehmE
+GiDQ/Fx2uWJilW59wMORYMfbTyDEnIWI55/LXuO/bS4DIzG3OKUS4O5emmXF1jmj
+5sekRDnhlq+ei0bO6Y+WhXZm5LQwQ/XVYXT4Bj4mRVXH1AJIvvtW4bU6bcLzzSFT
+pL0hPrqu4ReH9uCjSwSZ7iLQW5GPNojMBEtjtQgGswxZdmnY7XyfI8nxNWrWJM6w
+vwArpBo+hrodyliIzKQ76q2S5w6bRgJms6yC2Ezh1wKghnIJOBB8vR6KCXpaLGP3
+Zu0eT90CgYEA97PYbm2JkUNd0cyvGVSwsEC+uU3eJvCsggp/nv3tNWwGdjKL22nb
+Egv+aCxKkAqqKKinDIjLeQgDsY0ciy0IK0XrvEaUXVXr5uQLHsXSaBQOmbzUVPMb
+96qdBGihudlU2MlPkVce4XilAb+UdJjQ7WYHWNFPVxHMEQG5Cowpu4cCgYEAveci
+CY9oW7jOh1l58rh9YF4IknXN2dEIEo2R015EfdpH/sTGdq6LsBs4RJd3hBJIOySN
+qYvQ7ZEcsmF52MoknUuuE1XxTtEHipV4pRXjsMTkQQJQ95u0m2p4VQ0hlXffVmsa
+T5wuxyHbUJ0C80HDPZaqG1ca5ZRT163p5jHX5+UCgYEAvGt1qypvhqwz5a9JUn91
+l9zoN5Tf88cHsjPCOHL3ybdOgwKYGGNBXUz6TMvEPCWgoiUITSSRgpNyG9E3cTbk
+C+a4FxOpMAZ3176IJvRbxQfcw108NxJmp9KEdiIRQ4nqXK1ldS1MND7NZbMRfk/w
+PWVrGUi+jYDgNd+VnkxYQmkCgYEArXSx1VOk+aqLEnv69GXpNx2M7OInfV395pxi
+51LCcsXvXtizgnIZ62hhc9158CuWyFayAnxQAL68HH8GgarZ2MbooIaum9Q39DBk
++z1MNQkSiAKqnBAf3plYgi81TsYoPFIlvTOmUoMtRKu1pyWZROcaR1C/o5VhI66Z
+54X9dLUCgYEAlwOv5sDoqRneE7eAgBipq72u7w+WPiu3waaghpgATtKYHjm30M4A
+NV8ccw/9SIsyXW9Sx7aY5v729b4a3bp/IHVN20o8LddvFw+K13Jf3Fsv2SpuV6Qd
+mgCHj8977Oi/4Taf8XarfrJDEFodWYc9qzw3co6eYdQgQUSMLePghNQ=
+-----END RSA PRIVATE KEY-----
 
 #define IO_KEY       "aio_AbCD37pXDJeJvBsNuyf6l6RYqoCb"
